c63966718e0d5f1d7802ffb4e4d1fc7fefade23d
leeroy <leeroy.uziel.gg@outlook.es>
2021-09-28 09:20:00 -0500

    test2

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -3,7 +3,7 @@
     import Header from "../components/Header.svelte";
     import Main from "../components/Main.svelte";
     import TimeLine from "../components/TimeLine.svelte";
-    import Sidebar from "../components/SideBar.svelte";
+    import Sidebar from "../components/Sidebar.svelte";
 
     let data = {};
     const API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
