3f1a6887d2b0547387792f2151ffd8b437aa66a3
leeroy <leeroy.uziel.gg@outlook.es>
2021-09-28 09:00:05 -0500

    netlify

---
+++ src/components/Loading.svelte
@@ -0,0 +1,5 @@
+<div class="Loading">
+    <div class="Loading-container">
+        <p>Loading...</p>
+    </div>
+</div>
---
+++ src/components/TimeLine.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Card from "./Card.svelte";
+    import Loading from "./Loading.svelte";
+
+    export let posts = [];
+</script>
+
+<div class="TimeLine">
+    <div class="TimeLine-container">
+        {#each posts as post}
+            <Card {...post} />
+        {:else}
+            <Loading />
+        {/each}
+    </div>
+</div>
+
+<style>
+    .TimeLine {
+        padding: 4em 0 0 0;
+    }
+</style>
